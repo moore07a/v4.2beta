40bac12c36f8a5f6209b6d7f71cb00939213e93f
moore07a <168810166+moore07a@users.noreply.github.com>
2025-10-29 07:50:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js — AES redirector + Cloudflare Turnstile, hardened (v4.7 Advance Beta)
+// server.js — AES redirector + Cloudflare Turnstile, hardened (v4.7 Advance Beta Arranged)
 require("dotenv").config();
 const express = require("express");
 const crypto = require("crypto");
